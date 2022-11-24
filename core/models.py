9478d9df92f6923a7a30ca9027c50f3cb9e1fc3c
DiegoDalmolini <diegosfs_ufsc@hotmail.com>
2022-11-24 20:15:29 -0300

    feat: models feedbacks

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Feedbacks(models.Model):
+    
+    TYPES_CHOICES = (
+        ('positivo', 'Positivo'),
+        ('corretivo', 'Corretivo'),
+        ('redirecionador', 'Redirecionador'),
+        ('outros', 'Outros')
+    )
+
+    type = models.CharField('Tipo', max_length=14, choices=TYPES_CHOICES, help_text='Escolha o tipo do feedback.')
+    description = models.CharField('Descrição', max_length=200, help_text='Descreva a situação da ocorrência.')
+    action = models.CharField('Ação', max_length=200, help_text='Descreva a ação tomada.')
+
+
