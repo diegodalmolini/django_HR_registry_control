a97749b3cff9ec17a58b81c32eb4377b500ec369
DiegoDalmolini <diegosfs_ufsc@hotmail.com>
2022-11-24 20:19:15 -0300

    feat: models Epis

--- core/models.py
+++ core/models.py
@@ -14,3 +14,15 @@ class Feedbacks(models.Model):
     action = models.CharField('Ação', max_length=200, help_text='Descreva a ação tomada.')
 
 
+class Epis(models.Model):
+    
+    EPI_CHOICES = (
+        ('oculos', 'Óculos de segurança'),
+        ('luva', 'Luva de proteção'),
+        ('creme', 'Creme de proteção'),
+        ('sapato', 'Sapato de segurança')
+    )
+
+    epi = models.CharField('EPI', max_length=6, choices=EPI_CHOICES, help_text='Escolha o tipo de EPI.')
+    last_give = models.DateField('Data de entrega', max_length=9, help_text='Data da ultima entrega de EPI')
+    next_change = models.DateField('Data de troca', max_length=9, help_text='Data da próxima troca de EPI') 
