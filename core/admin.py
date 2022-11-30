aa450227b27c79961715bd9a929e8c8f43bd0aa4
DiegoDalmolini <diegosfs_ufsc@hotmail.com>
2022-11-30 02:08:48 -0300

    feat: Template bootstrap

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Feedbacks,
+    Epis,
+    Benefits,
+    Scholarity,
+    Dependents,
+    Roles,
+    Departments,
+    People,
+    Trainings,
+    Employee
+)
+
+
+admin.site.register(Feedbacks)
+admin.site.register(Epis)
+admin.site.register(Benefits)
+admin.site.register(Scholarity)
+admin.site.register(Dependents)
+admin.site.register(Roles)
+admin.site.register(Departments)
+admin.site.register(People)
+admin.site.register(Trainings)
+admin.site.register(Employee)
--- core/models.py
+++ core/models.py
@@ -13,6 +13,11 @@ class Feedbacks(models.Model):
     description = models.TextField('Descrição', max_length=200, help_text='Descreva a situação da ocorrência.')
     action = models.TextField('Ação', max_length=200, help_text='Descreva a ação tomada.')
 
+    
+    class Meta:
+        verbose_name = 'Feedback'
+        verbose_name_plural = 'Feedbacks'
+
     def __str__(self):
         return self.type
 
@@ -29,6 +34,11 @@ class Epis(models.Model):
     last_give = models.DateField('Data de entrega', max_length=9, help_text='Data da ultima entrega de EPI')
     next_change = models.DateField('Data de troca', max_length=9, help_text='Data da próxima troca de EPI')
 
+    
+    class Meta:
+        verbose_name = 'EPI'
+        verbose_name_plural = 'EPIs'
+
     def __str__(self):
         return f'{self.epi} - {self.last_give} > {self.next_change}'
 
@@ -36,16 +46,24 @@ class Epis(models.Model):
 class Benefits(models.Model):
 
     BENEFITS_CHOICES = (
-        ('saude', 'Plano de saúde'),
-        ('odontologico', 'Plano odontológico')
+        ('saude/personal', 'Plano de saúde/Personal'),
+        ('saude/nacional', 'Plano de saúde/Nacional'),
+        ('odontologico/c/protese', 'Plano odontológico com prótese'),
+        ('odontologico/s/protese', 'Plano odontológico sem prótese'),
     )
 
-    benefit = models.CharField('Benificio', max_length=12, help_text='Escolha o tipo de plano:')
+
+    benefit = models.CharField('Benificio', max_length=22, choices=BENEFITS_CHOICES , help_text='Escolha o tipo de plano:')
     user = models.CharField('Usuário', max_length=50, help_text='Escolha o usuário que irá utilizar o plano.')
     value = models.FloatField('Valor', help_text='Informe o valor do plano.')
 
+    
+    class Meta:
+        verbose_name = 'Benefício'
+        verbose_name_plural = 'Benefícios'
+
     def __str__(self):
-        return f'{self.user} - {self.benefit} - {self.value}'
+        return f'{self.user} - {self.benefit} - R${self.value}'
 
 class Scholarity(models.Model):
 
@@ -68,6 +86,11 @@ class Scholarity(models.Model):
     conclusion_date = models.DateField('Data de conclusão', max_length=6, help_text='MM/AA')
     situation = models.CharField('Situação', max_length=10, choices=SITUATION_CHOICES)
 
+    class Meta:
+        verbose_name = 'Escolaridade'
+        verbose_name_plural = 'Escolaridades'
+
+
     def __str__(self):
         return f'{self.degree} - {self.situation}'
 
@@ -78,6 +101,11 @@ class Dependents(models.Model):
     last_name = models.CharField('Sobrenome', max_length=50, help_text='Sobrenome do dependente.')
     age = models.IntegerField('Idade', help_text='Idade do dependente.')
     relationship = models.CharField('Parentesco', max_length=50, help_text='O parentesco em relação ao colaborador.')
+
+    class Meta:
+        verbose_name = 'Dependente'
+        verbose_name_plural = 'Dependentes'
+
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} - {self.relationship}'
@@ -108,24 +136,6 @@ class Departments(models.Model):
     def __str__(self):
         return f'{self.name}'
 
-class Labor_data(models.Model):
-
-    SHIFT_CHOICES = (
-        ('primeiro','Primeiro Turno'),
-        ('segundo','Segundo Turno'),
-        ('terceiro','Terceiro Turno'),
-        ('normal','Normal'),
-    )
-
-    admission = models.DateField('Admissâo', max_length=9, help_text='Data de admissão do colaborador.')
-    role = models.ForeignKey(Roles, verbose_name='Cargo', on_delete=models.CASCADE)
-    cbo = models.CharField('CBO', max_length=7, help_text='Informe o número do CBO.')
-    department = models.ForeignKey(Departments, verbose_name='Setor', on_delete=models.CASCADE)
-    salary = models.FloatField('Salário')
-    superior = models.CharField('Superior imediato', max_length=50)
-    shift = models.CharField('Turno', max_length=8, choices=SHIFT_CHOICES)
-    last_exam = models.DateField('Ultimo exame', max_length=9, help_text='Data do ultimo exame.')
-    next_exam = models.DateField('Próximo exame', max_length=9, help_text='Data do próximo exame.')
 
 
 class Trainings(models.Model):
@@ -137,29 +147,66 @@ class Trainings(models.Model):
     start_date = models.CharField('Data do inicio', max_length=9)
     finish_date = models.CharField('Data de conclução', max_length=9)
 
+    class Meta:
+        verbose_name = 'Treinamento'
+        verbose_name_plural = 'Treinamentos'
+
+
     def __str__(self):
         return self.name
 
 
-class Employee(models.Model):
-
+class People(models.Model):
     first_name = models.CharField('Nome', max_length=50)
     last_name = models.CharField('Sobrenome', max_length=50)
     rg = models.IntegerField('RG')
     cpf = models.CharField('CPF', max_length=14)
-    pis = models.CharField('PIS', max_length=14)
-    ct_number = models.IntegerField('Número da carteira de trabalho')
-    ct_series = models.IntegerField('Série da carteira de trabalho')
     birthday = models.DateField('Data de nascimento', max_length=9)
-    internal_code = models.IntegerField('Código interno')
     adress = models.CharField('Endereço', max_length=100)
     phone = models.CharField('Telefone', max_length=15)
     father_name = models.CharField('Nome do pai', max_length=50)
     mother_name = models.CharField('Nome da mãe', max_length=50)
+
+    class Meta:
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Employee(models.Model):
+
+    SHIFT_CHOICES = (
+        ('primeiro','Primeiro Turno'),
+        ('segundo','Segundo Turno'),
+        ('terceiro','Terceiro Turno'),
+        ('normal','Normal'),
+    )
+    admission = models.DateField('Admissâo', max_length=9, help_text='Data de admissão do colaborador.')
+    role = models.ForeignKey(Roles, verbose_name='Cargo', on_delete=models.CASCADE)
+    cbo = models.CharField('CBO', max_length=7, help_text='Informe o número do CBO.')
+    department = models.ForeignKey(Departments, verbose_name='Setor', on_delete=models.CASCADE)
+    salary = models.FloatField('Salário')
+    superior = models.CharField('Superior imediato', max_length=50)
+    shift = models.CharField('Turno', max_length=8, choices=SHIFT_CHOICES)
+    last_exam = models.DateField('Ultimo exame', max_length=9, help_text='Data do ultimo exame.')
+    next_exam = models.DateField('Próximo exame', max_length=9, help_text='Data do próximo exame.')
+    pis = models.CharField('PIS', max_length=14)
+    ct_number = models.IntegerField('Número da carteira de trabalho')
+    ct_series = models.IntegerField('Série da carteira de trabalho')
+    internal_code = models.IntegerField('Código interno')
     dependents = models.ForeignKey(Dependents, verbose_name='Dependentes', on_delete=models.CASCADE)
-    labor_data = models.OneToOneField(Labor_data, verbose_name='Dados laborais', on_delete=models.CASCADE)
+    people = models.OneToOneField(People, verbose_name='Pessoas', on_delete=models.CASCADE)
     trainings = models.ForeignKey(Trainings, verbose_name='Treinamentos', on_delete=models.CASCADE)
     feedbacks = models.ForeignKey(Feedbacks, verbose_name='Feedbacks', on_delete=models.CASCADE)
     epis = models.ForeignKey(Epis, verbose_name='EPIs', on_delete=models.CASCADE)
     benefits = models.ForeignKey(Benefits, verbose_name='Benifícios', on_delete=models.CASCADE)
     scholarity = models.ForeignKey(Scholarity, verbose_name='Escolaridade', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Funcionario'
+        verbose_name_plural = 'Funcionarios'
+
+    def __str__(self):
+        return f'{self.people.first_name} {self.people.last_name} - Cargo: {self.role}'
