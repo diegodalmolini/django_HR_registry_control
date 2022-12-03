734ba62e1a8b1ffaa16c2bbc151ff715b3d617ca
DiegoDalmolini <diegosfs_ufsc@hotmail.com>
2022-12-03 20:55:20 -0300

    feat: hyperlink for employye detailview in employee List

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import HomeView, EmployeeListView
+from .views import HomeView, EmployeeListView, EmployeeDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
-    path('funcionarios', EmployeeListView.as_view(), name='funcionarios')
+
+    path('funcionarios', EmployeeListView.as_view(), name='funcionarios'),
+
+    path('funcionario/<int:pk>', EmployeeDetailView.as_view(), name='funcionario')
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Employee
 
 class HomeView(TemplateView):
@@ -9,3 +9,7 @@ class EmployeeListView(ListView):
     model = Employee
     template_name = 'employee-list.html'
 
+
+class EmployeeDetailView(DetailView):
+    model = Employee
+    template_name = 'employee-detail.html'
